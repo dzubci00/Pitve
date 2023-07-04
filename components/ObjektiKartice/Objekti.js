39d648bf68417c09cf50a7003b78b64e1cc4c2b7
dzubci00 <48828447+dzubci00@users.noreply.github.com>
2023-07-04 13:15:32 +0200

    Update Objekti.js

--- components/ObjektiKartice/Objekti.js
+++ components/ObjektiKartice/Objekti.js
@@ -55,8 +55,8 @@ const Objekti = () => {
     infinite:true,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 0,
-    speed:10000,
+    autoplaySpeed: -10000,
+    speed:5000,
     pauseOnHover: false,
     cssEase: 'linear',
     responsive: [
