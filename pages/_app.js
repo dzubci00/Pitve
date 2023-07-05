c51eb267171ca1bc1a2f930dd45b52f073c3cbd7
Donat Zubcic <dzubci00@fesb.hr>
2023-07-05 13:14:53 +0200

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 // import "../styles/globals.css";
 import "../styles/index.css";
+/* import ReactGA from "react-ga";
+
+ReactGA.initialize('YOUR_TRACKING_ID');
+
+//id G-E5S2YTWC0T
+// Track pageview on each route change
+Router.events.on('routeChangeComplete', (url) => {
+  ReactGA.pageview(url);
+}); */
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
